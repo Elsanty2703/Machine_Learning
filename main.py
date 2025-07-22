6952ec2746e8db2500985824360e3f13716a21c4
Santiago Gamboa <elsanty_2703@Santiagos-MacBook-Pro.local>
2025-07-22 10:10:02 -0500

    Ahora si un ecosistema

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+
+print("Usando este Python:", sys.executable)
+
+print("Hola, este es mi entorno Conda funcionando 😎")
+
+a = np.array([1, 2, 3])
+print("Este es un arreglo de NumPy:", a)
